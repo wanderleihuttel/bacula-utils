a8b6ee063ba1fac3e0f926db3ef2f3a886708127
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-09-12 15:44:58 -0300

    Modify verify script

--- conf/scripts/_verify_jobs_mysql.sh
+++ conf/scripts/_verify_jobs_mysql.sh
@@ -9,6 +9,8 @@ DBNAME="bacula"
 DBUSER="bacula"
 DBPASSWD="bacula"
 
+BCONSOLE="/sbin/bconsole"
+MYSQL="/usr/bin/mysql"
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Check day of Week
@@ -23,24 +25,33 @@ fi
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Verify Jobs Used
-verify_array=( "Verify_Data" "Verify_VolumeToCatalog") 
-
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # SQL query to get data from Job (MySQL)
-sql_query="SELECT Job.JobId, Job.Name, Client.Name as Client, FileSet.FileSet,
-(SELECT DISTINCT Storage.Name FROM JobMedia LEFT JOIN Media ON (JobMedia.MediaId = Media.MediaId) 
-LEFT JOIN Storage ON (Media.StorageId = Storage.StorageId)  WHERE JobMedia.JobId = Job.JobId
-) AS Storage 
-FROM Job, Client, FileSet
-WHERE Job.ClientId = Client.ClientId and Job.FileSetId = FileSet.FileSetId
-AND Job.EndTime > (NOW() - INTERVAL $NUMBEROFDAYS DAY) AND Job.JobStatus = 'T' ORDER BY RAND() LIMIT 5;"
+if [ $# -eq 1 ]; then
+   sql_query="SELECT Job.JobId, Job.Name, Client.Name as Client, FileSet.FileSet,
+   (SELECT DISTINCT Storage.Name FROM JobMedia LEFT JOIN Media ON (JobMedia.MediaId = Media.MediaId)
+   LEFT JOIN Storage ON (Media.StorageId = Storage.StorageId)  WHERE JobMedia.JobId = Job.JobId
+   ) AS Storage
+   FROM Job, Client, FileSet
+   WHERE Job.ClientId = Client.ClientId and Job.FileSetId = FileSet.FileSetId
+   AND Job.JobStatus = 'T' AND Job.Jobid = $1";
+else
+   sql_query="SELECT Job.JobId, Job.Name, Client.Name as Client, FileSet.FileSet,
+   (SELECT DISTINCT Storage.Name FROM JobMedia LEFT JOIN Media ON (JobMedia.MediaId = Media.MediaId) 
+   LEFT JOIN Storage ON (Media.StorageId = Storage.StorageId)  WHERE JobMedia.JobId = Job.JobId
+   ) AS Storage 
+   FROM Job, Client, FileSet
+   WHERE Job.ClientId = Client.ClientId and Job.FileSetId = FileSet.FileSetId
+   AND Job.EndTime > (NOW() - INTERVAL $NUMBEROFDAYS DAY) AND Job.JobStatus = 'T' ORDER BY RAND() LIMIT 5;"
+fi
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Verify Jobs Used
+#verify_array=( "Verify_Data" "Verify_VolumeToCatalog") 
+verify_array=( "Verify_Data") 
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Execute SQL and get data from Job table
-query=`echo "$sql_query" | mysql -u $DBUSER -p$DBPASSWD -D $DBNAME -h $DBHOST -P $DBPORT  -N -t`
+query=`echo "$sql_query" | $MYSQL -u $DBUSER -p$DBPASSWD -D $DBNAME -h $DBHOST -P $DBPORT  -N -t`
 c=10
 
 echo "$query" | grep -v "\-\-\-" | while read row; do
@@ -50,13 +61,13 @@ echo "$query" | grep -v "\-\-\-" | while read row; do
   Fileset=`echo $row | cut -d"|" -f5 | sed 's/^ \(.*\) $/\1/'`;
   Storage=`echo $row | cut -d"|" -f6 | sed 's/^ \(.*\) $/\1/'`;
   Verify_Job=`printf '%s\n' "${verify_array[@]}" | shuf -n1`
-  JobIdRun=`echo "run job=$Verify_Job  client=$Client  fileset=$Fileset  jobid=$JobId  storage=$Storage  priority=$c  yes" | bconsole | grep "JobId" | sed 's/[^0-9]*//g'`
+  JobIdRun=`echo "run job=$Verify_Job  client=$Client  fileset=$Fileset  jobid=$JobId  storage=$Storage  priority=$c  yes" | $BCONSOLE | grep "JobId" | sed 's/[^0-9]*//g'`
   # Execute the job in bconsole
   echo "run job=$Verify_Job  client=$Client  fileset=$Fileset  jobid=$JobId  storage=$Storage  priority=$c  yes"
   (( c++ ))
   sleep 1 
   # The MySQL command update Job name including the Job that was verified
   if [ "$JobIdRun" != "" ]; then
-     mysql -u $DBUSER -p$DBPASSWD -D $DBNAME -h $DBHOST -P $DBPORT -e "update Job set Name = '$Verify_Job ($JobName)' where JobId = $JobIdRun;"
+     $MYSQL -u $DBUSER -p$DBPASSWD -D $DBNAME -h $DBHOST -P $DBPORT -e "update Job set Name = '$JobName (V)' where JobId = $JobIdRun;"
   fi
 done
