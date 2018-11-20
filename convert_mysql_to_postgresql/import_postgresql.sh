cca590fbba4373267370fc0d8dd40ebdc6cfa8b5
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2018-11-20 14:29:29 -0200

    - Applied some suggestions given by @anchev - Remove word _binary in MySQL dump 5.7 - Modify elapsed time

--- convert_mysql_to_postgresql/import_postgresql.sh
+++ convert_mysql_to_postgresql/import_postgresql.sh
@@ -10,16 +10,14 @@
 # localhost:5432:bacula:bacula:bacula
 # end of file
 
-dir_backup="/tmp/bacula"
-sequence="${dir_backup}/sequence.sql"
+sequence="sequence.sql"
 db_user="bacula"
 db_name="bacula"
-row_count="${dir_backup}/RowCountPg.log"
-psql=`which psql`
-#tables="BaseFiles CDImages Client Counters Device File FileSet Filename Job JobHisto JobMedia Location LocationLog Log Media MediaType Path PathHierarchy PathVisibility Pool RestoreObject Snapshot Status Storage UnsavedFiles Version"
+row_count="RowCountPg.log"
+psql=$(which psql)
+# List of Bacula Tables
+# tables (Status and Version) are not imported, they are generated during make_postgresql_tables script
 tables="BaseFiles CDImages Client Counters Device File FileSet Filename Job JobHisto JobMedia Location LocationLog Log Media MediaType Path PathHierarchy PathVisibility Pool RestoreObject Snapshot Storage UnsavedFiles"
-
-cd ${dir_backup}
 
 #######################################
 # Start Import
@@ -27,19 +25,23 @@ echo "-----------------------------------------------"
 echo " Import Bacula Catalog from MySQL to PostgreSQL"
 echo " Author:  Wanderlei Hüttel"
 echo " Email:   wanderlei.huttel@gmail.com"
-echo " Version: 1.1 - 25/10/2017"
+echo " Version: 1.3 - 20/11/2018"
 echo "-----------------------------------------------"
-starttimeg=`date +%s`
+echo
+echo "Importing MySQL tables ..."
+echo "--------------------------------------------"
+echo "Table           Time"
+echo "--------------------------------------------"
+
+totalstarttime=$(date -u +%s)
 for table in ${tables}; do 
    log="${table}.log"
-   starttime=`date +%s`
+   starttime=$(date -u +%s)
    ${psql} -U ${db_user} -d ${db_name} -f "${table}.sql" > /dev/null 2>> ${log}
-   endtime=`date +%s`
-   totaltime=`expr ${endtime} - ${starttime} + 10800`
-   printf "Time to restore table %-20s %8s\n" ${table} `date -d @${totaltime} +%H:%M:%S`
+   endtime=$(date -u +%s)
+   elapsedtime=$(expr ${endtime} - ${starttime})
+   printf "%-15s %8s\n" ${table} $(date -ud @${elapsedtime} +%H:%M:%S)
 done
-endtimeg=`date +%s`
-totaltimeg=`expr ${endtimeg} - ${starttimeg} + 10800`
 
 
 #######################################
@@ -65,15 +67,24 @@ SELECT SETVAL('snapshot_snapshotid_seq', (SELECT MAX(snapshotid) FROM snapshot))
 SELECT SETVAL('storage_storageid_seq', (SELECT MAX(storageid) FROM storage));
 " > ${sequence}
 ${psql} -U ${db_user} -d ${db_name} -f ${sequence} > /dev/null 2>> sequence.log
-printf "Total time to restore all tables %18s\n" `date -d @${totaltimeg} +%H:%M:%S`
+totalendtime=$(date -u +%s)
+totalelapsedtime=$(expr ${totalendtime} - ${totalstarttime})
+
+echo "--------------------------------------------"
+echo "Elapsed time:   $(date -ud @${totalelapsedtime} +%H:%M:%S)"
+echo "--------------------------------------------"
+echo 
+echo 
+echo "" > ${row_count}
 
 #######################################
 # Count rows by tables
 tables="BaseFiles CDImages Client Counters Device File FileSet Filename Job JobHisto JobMedia Location LocationLog Log Media MediaType Path PathHierarchy PathVisibility Pool RestoreObject Snapshot Status Storage UnsavedFiles Version"
-echo "0|PostgreSQL" > ${row_count}
-echo "-----------------------------------------------"
-echo "Total row count tables"
-echo "-----------------------------------------------"
+echo "Total tables row count"
+echo "--------------------------------------------"
+echo "Table           Row Count"
+echo "--------------------------------------------"
+
 sql_query="select table_name  from information_schema.tables where table_catalog = 'bacula' and table_schema = 'public' and table_name not like 'webacula_%' order by table_name"
 for table in ${tables}; do 
    sql_query="select '$table' tabela, count(*) total from $table;"; 
@@ -82,11 +93,18 @@ done
 sed -i 's/\s//g' ${row_count}
 sed -i '/^$/d' ${row_count}
 sort ${row_count} -o ${row_count}
-sed -i 's/0|PostgreSQL/PostreSQL/g' ${row_count}
-cat ${row_count}
-
+output=$(<${row_count})
+output=$(echo -e $output | sed 's/[|]/\t/g')
+printf "%-15s %8s\n" $output
+echo "--------------------------------------------"
+echo
+echo
 
 #######################################
 # Update break line in table LogText
+echo "--------------------------------------------"
+echo "Updating break line in table LogText..."
 ${psql} -U ${db_user} ${db_name} -c "UPDATE Log SET LogText = replace(LogText,'\n',E'\n');";
-
+echo "--------------------------------------------"
+echo 
+echo "Done"
--- convert_mysql_to_postgresql/mysql_to_postresql.sh
+++ convert_mysql_to_postgresql/mysql_to_postresql.sh
@@ -2,74 +2,102 @@
 
 #######################################
 # Config Parameters
-db_user="root"
+db_username="root"
 db_password=""
 db_name="bacula"
-dir_backup="/tmp/bacula"
-row_count="${dir_backup}/RowCountMySQL.log"
+db_host="localhost"
+row_count="RowCountMySQL.log"
 
-mysql=`which mysql`
-mysqldump=`which mysqldump`
+echo "--------------------------------------------"
+echo " Convert Bacula Catalog MySQL to PostgreSQL"
+echo " Author:  Wanderlei Hüttel"
+echo " Email:   wanderlei.huttel@gmail.com"
+echo " Version: 1.3 - 20/11/2018"
+echo
+echo "--------------------------------------------"
+echo "Please inform your database credentials..."
+read -p  "MySQL db_name:     " -i "${db_name}" -e db_name
+read -p  "MySQL db_username: " -i "${db_username}" -e db_username
+read -p  "MySQL db_host:     " -i "${db_host}" -e db_host
+read -p "MySQL db_password ${db_username}: " -s db_password
+echo 
+echo "--------------------------------------------"
 
+mysql=$(which mysql)
+mysqldump=$(which mysqldump)
+
+# List of Bacula Tables
 tables="BaseFiles CDImages Client Counters Device File FileSet Filename Job JobHisto JobMedia Location LocationLog Log Media MediaType Path PathHierarchy PathVisibility Pool RestoreObject Snapshot Status Storage UnsavedFiles Version"
 
 #######################################
 # Check if the password is not empty
 if [ ! -z ${db_password} ]; then
-  db_password="-p${db_password}"
+    db_password="-p${db_password}"
 fi
 
 #######################################
-# 
+#
+echo
+echo "Dumping MySQL tables ..."
 echo "--------------------------------------------"
-echo " Convert Bacula Catalog MySQL to PostgreSQL"
-echo " Author:  Wanderlei Hüttel"
-echo " Email:   wanderlei.huttel@gmail.com"
-echo " Version: 1.1 - 25/10/2017"
+echo "Table           Time"
 echo "--------------------------------------------"
-starttimeglobal=`date +%s`
+totalstarttime=$(date -u +%s)
 for table in ${tables}; do
-   starttime=`date +%s`
+    starttime=$(date -u +%s)
 
-   echo "\set AUTOCOMMIT OFF" >> "${table}.sql"
-   ${mysqldump} --no-create-info \
-                --no-create-db \
-                --complete-insert \
-                --compatible=postgresql \
-                --skip-quote-names \
-                --disable-keys \
-                --lock-tables \
-                --compact \
-                --skip-opt \
-                --quick \
-                -u ${db_user} ${db_password} ${db_name} ${table} | \
-                sed -e 's/0000-00-00 00:00:00/1970-01-01 00:00:00/g' | \
-                sed -e 's/\\0//g' | \
-                sed -e 's/\\"/"/g' | \
-                sed -e 's/\\\\/\\/g' | \
-                sed -e "s/\\\'/\'\'/g" >> "${table}.sql"
-                echo "COMMIT;" >> "${table}.sql"
+    echo "\set AUTOCOMMIT OFF" >> "${table}.sql"
+    ${mysqldump} --no-create-info \
+                 --no-create-db \
+                 --complete-insert \
+                 --compatible=postgresql \
+                 --skip-quote-names \
+                 --disable-keys \
+                 --lock-tables \
+                 --compact \
+                 --skip-opt \
+                 --quick \
+                 -h ${db_host} -u ${db_username} ${db_password} ${db_name} ${table} | \
+                 sed -e 's/0000-00-00 00:00:00/1970-01-01 00:00:00/g' | \
+                 sed -e 's/\\0//g' | \
+                 sed -e 's/\\"/"/g' | \
+                 sed -e 's/\\\\/\\/g' | \
+                 sed -e "s/\\\'/\'\'/g" | \
+                 sed -e "s/_binary//g" \
+                 >> "${table}.sql"
+                 echo "COMMIT;" >> "${table}.sql"
 
-   endtime=`date +%s`
-   totaltime=`expr ${endtime} - ${starttime} + 10800`
-   printf "Dump table %-15s Time: %8s\n" ${table} `date -d @${totaltime} +%H:%M:%S`
+    endtime=$(date -u +%s)
+    elapsedtime=$(expr ${endtime} - ${starttime})
+    printf "%-15s %8s\n" ${table} $(date -ud @${elapsedtime} +%H:%M:%S)
 done
-endtimeglobal=`date +%s`
-totaltimeglobal=`expr ${endtimeglobal} - ${starttimeglobal} + 10800`
+totalendtime=$(date -u +%s)
+totalelapsedtime=$(expr ${totalendtime} - ${totalstarttime})
+echo "--------------------------------------------"
+echo "Elapsed time:   $(date -ud @${totalelapsedtime} +%H:%M:%S)"
+echo "--------------------------------------------"
+echo
+echo
 echo "" > ${row_count}
-echo "--------------------------------------------"
-echo "Total row count tables"
-echo "--------------------------------------------"
-echo "0|MySQL" > ${row_count}
+
+#######################################
+# Count rows by tables
 for table in ${tables} ; do
-   sql_query="select '$table' as tabela, count(*) as total from $table;"
-   ${mysql} -u ${db_user} -D ${db_name} ${db_password} -N  -B -e "$sql_query" | sed 's/\t/\|/g' | tr '[:upper:]' '[:lower:]' >> ${row_count}
+    sql_query="select '$table' as tabela, count(*) as total from $table;"
+    ${mysql} -h ${db_host} -u ${db_username} -D ${db_name} ${db_password} -N  -B -e "$sql_query" \
+    | sed 's/\t/\|/g' | tr '[:upper:]' '[:lower:]' >> ${row_count}
 done
+
 sed -i 's/\s//g' ${row_count}
 sed -i '/^$/d' ${row_count}
 sort ${row_count} -o ${row_count}
-sed -i 's/0|MySQL/MySQL/g' ${row_count}
-cat ${row_count}
+echo "Total tables row count"
 echo "--------------------------------------------"
-echo "Total dump tables          Time: `date -d @${totaltimeglobal} +%H:%M:%S`"
+echo "Table           Row Count"
 echo "--------------------------------------------"
+output=$(<${row_count})
+output=$(echo -e $output | sed 's/[|]/\t/g')
+printf "%-15s %8s\n" $output
+echo "--------------------------------------------"
+echo
+echo "Done"
