3e85f663dac7d40996e86d31fa7490de0b8cb5fc
Wanderlei <wanderleihuttel@users.noreply.github.com>
2020-03-09 14:21:32 -0300

    Update _disable_backup_holidays.sh

--- conf/scripts/_disable_backup_holidays.sh
+++ conf/scripts/_disable_backup_holidays.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
-# Script to disable schedule in holidays
 
-# Crontab
+#===============================================
+# Script to disable schedule on holidays
+# Author:  Wanderlei Hüttel
+# Email:   wanderlei.huttel@gmail.com
+# Version: 1.1 - 09/03/2020
+#===============================================
+
+# Put the execution in Crontab
 # 00 18 * * 1-7 /etc/bacula/scripts/_disable_backup_holidays.sh > /dev/null
 
-# Define an array of holidays (%m-%d)
-array_holidays=( "01-01" "03-25" "03-27" "04-21" "05-01" "05-26" "09-07" "10-12" "11-02" "11-15" "12-25")
+bconsole=$(which bconsole)
 today=$(date +%m-%d)
-for holiday in "${array_holidays[@]}"; do
-   if [ "${holiday}" == "{$today}" ]; then
-      for j in $(echo ".schedule" | bconsole | sed '1,4d;' | grep -v "You have messages"); do 
-         echo "disable schedule=${j}" | bconsole > /dev/null
-         echo "The schedule \"${j}\" was disabled today!"
-      done
-      exit 0
-   fi
+
+# Ann array with holidays (%m-%d)
+holidays=("01-01, 03-25, 03-27, 04-21, 05-01, 05-26, 09-07, 10-12, 11-02, 11-15, 12-25")
+
+# Get schedules from Bacula
+schedules=$(echo ".schedule" | ${bconsole} | sed '1,4d;' | grep -v "You have messages")
+
+# Disable schedules
+for holiday in $(echo "${holidays}" | tr "," "\n"); do
+    # Check if today is a holiday
+    if [ "${holiday}" == "${today}" ]; then
+        echo -e "Today is a holiday! All schedules were disabled!\n"
+        # Disable as schedules and finish
+        for schedule in $(echo ${schedules}); do
+            echo "disable schedule=${schedule}" | ${bconsole} > /dev/null
+            echo "The schedule \"${schedule}\" are disable in ${today}!"
+        done
+        exit 0
+    fi
 done
-echo "reload" | bconsole > /dev/null
-echo "Is not holiday today! Bacula will execute normal!"
+
+
+# Enable schedules
+echo -e "Is not holiday today! All schedules were enabled!\n"
+for schedule in $( echo ${schedules} ); do
+    echo "enable schedule=${schedule}" | ${bconsole} > /dev/null
+    echo "The schedule \"${schedule}\" are enable in ${today}!"
+done
+
 exit 0
