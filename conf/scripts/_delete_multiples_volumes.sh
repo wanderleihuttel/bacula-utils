6274feb9583be97299ad983d2653a07c96b27499
Wanderlei Hüttel <wanderleihuttel@users.noreply.github.com>
2022-04-11 11:39:31 -0300

    Update _delete_multiples_volumes.sh

--- conf/scripts/_delete_multiples_volumes.sh
+++ conf/scripts/_delete_multiples_volumes.sh
@@ -3,44 +3,44 @@
 bconsole=$(which bconsole)
 
 clear
-echo "--------------------------------------------"
-echo " Script to delete volumes in mass"
-echo " Author:  Wanderlei Hüttel"
-echo " Email:   wanderlei.huttel@gmail.com"
-echo " Version: 1.2 - 09/03/2020"
-echo "--------------------------------------------"
-echo ""
+echo "
+ --------------------------------------------
+ Script to delete volumes in mass
+ Author:  Wanderlei Hüttel
+ Email:   wanderlei.huttel@gmail.com
+ Version: 1.3 - 11/04/2022
+ --------------------------------------------
+"
 
-echo "list pools" | ${bconsole} | grep "[+|\|]"
-read -p "Enter the name of the Pool that you would like to delete the volumes: " pool
-echo "Selected Pool: ${pool}"
-echo ""
-read -p "Enter the status of the volumes you would like to check (Error, Used, Full, Append, Recycled): " volstatus
+$bconsole <<< "list pools" | grep "[+|\|]"
+read -p "Enter the [NAME OF THE POOL] that you would like to delete the volumes: " pool
+echo -e "Selected Pool: ${pool}\n"
+read -p "Enter the [STATUS OF THE VOLUME] that you would like to check (Error, Used, Full, Append, Recycled): " volstatus
 
-arraycount=$(echo "list media pool=${pool}" | ${bconsole}  | grep "|" | grep -v "MediaId" | grep "${volstatus}" | cut -d "|" -f3 | sed 's/ //g' | wc -l)
-echo ""
-for volname in $(echo "list media pool=${pool}" | ${bconsole}  | grep "|" | grep -v "MediaId" | grep "${volstatus}" | cut -d "|" -f3 | sed 's/ //g'); do
-    echo " ${volname}" ;
+volumearray=($( $bconsole <<< "list media pool=${pool}" | grep "|" | grep -v "MediaId" | grep "$volstatus" | cut -d "|" -f3 | sed -E 's/\s+//g' ))
+echo
+for volumename in ${volumearray[@]}; do
+    echo " $volumename" ;
 done
-echo ""
+echo
 
-if [ ${arraycount} == "0" ]; then
-    echo -e "No volumes were found in the pool '${pool}' with status '${volstatus}'\n"
+if [[ ${#volumearray[@]} == "0" ]]; then
+    echo -e "No volumes found in the pool '$pool' with status '$volstatus'\n"
     echo -e "Operation aborted!\n"
     exit
 else
-    echo -e "${arraycount} volumes was found with status '${volstatus}'\n"
+    echo -e "${#volumearray[@]} volumes was found with status '$volstatus'\n"
 fi
 
-read -p "Are you sure you want to delete all ${arraycount} volumes? This operation is irreversible! (Y-Yes / N-No) " confirm
-if [ "${confirm}" == "y" ] || [ "${confirm}" == "Y" ]; then
-    for volname in $(echo "list media pool=${pool}" | ${bconsole}  | grep "|" | grep -v "MediaId" | grep "${volstatus}" | cut -d "|" -f3 | sed 's/ //g'); do
-        echo "delete volume=${volname} pool=${pool} yes" ;
+read -p "Are you sure you want to delete all ${#volumearray[@]} volumes? This operation is irreversible! (Y-Yes / N-No) " confirm
+if [[ "${confirm,,}" == "y" ]]; then
+     for volumename in ${volumearray[@]}; do
+        echo "delete volume=$volumename pool=$pool yes" ;
         # Uncomment the line below to exclude the volumes in the catalog
-        #echo "delete volume=${volname} pool=${pool} yes" | ${bconsole} ;
+        # $bconsole <<< "delete volume=$volumename pool=$pool yes"
 
         # Uncomment the line below and change the path of storage to remove volume from the disk also
-        # rm -f /path/to/storage/${volname}
+        # rm -f /path/to/storage/$volumename
     done
     echo -e "Operation finished with success!\n"
 else
